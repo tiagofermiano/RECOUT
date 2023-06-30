b76fd711f43b496e0fc06a6f4a98d10c6d861f21
celotwentythree <vlcelo2005@gmail.com>
2023-06-29 22:59:16 -0300

    linkando itens com o data base

--- server.js
+++ server.js
@@ -13,78 +13,113 @@ app.use(express.static('pages'));
 // CONEXÃO COM O BANCO
 
 const connection = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'root',
-    database: 'recout',
+  host: '127.0.0.1',
+  user: 'root',
+  password: 'root',
+  database: 'recout',
 });
 
 connection.connect(function (err) {
-    if (!err) {
-        console.log("Conexão como o Banco realizada com sucesso!!!");
-    } else {
-        console.log("Erro: Conexão NÃO realizada", err);
-    }
+  if (!err) {
+    console.log("Conexão como o Banco realizada com sucesso!!!");
+  } else {
+    console.log("Erro: Conexão NÃO realizada", err);
+  }
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/pages/recout_perfil.html')
+  res.sendFile(__dirname + '/pages/recout_perfil.html')
 })
 
 // FUNÇÕES PARA LOGIN
 // FUNÇÕES PARA LOGIN
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/pages/recout_perfil.html')
-  })
-  
-  app.get('/login', (req, res) => {
-    res.sendFile(__dirname + '/pages/recout_login.html')
-  })
-  app.get('/cadastro', (req, res) => {
-    res.sendFile(__dirname + '/pages/recout_cadastro.html')
-  })
-  app.post('/login', (req, res) => {
-    let email = req.body.email;
-    let password = req.body.password;
-  
-  
-    connection.query("SELECT * FROM cliente where email = '" + email +"'", function (err, rows) {
-      if (!err){
-        if (email === rows[0].email && password === rows[0].password ){
-          console.log('Senha OK');
-          res.redirect('/');
-        }else{
-          console.log('Senha errada');
-          alert('Email ou senha incorreto')
-        }
-  
-      }});
-  
-  })
+  res.sendFile(__dirname + '/pages/recout_perfil.html')
+})
+
+app.get('/login', (req, res) => {
+  res.sendFile(__dirname + '/pages/recout_login.html')
+})
+app.get('/cadastro', (req, res) => {
+  res.sendFile(__dirname + '/pages/recout_cadastro.html')
+})
+app.post('/login', (req, res) => {
+  let email = req.body.email;
+  let password = req.body.password;
+
+
+  connection.query("SELECT * FROM cliente where email = '" + email + "'", function (err, rows) {
+    if (!err) {
+      if (email === rows[0].email && password === rows[0].password) {
+        console.log('Senha OK');
+        res.redirect('/');
+      } else {
+        console.log('Senha errada');
+        alert('Email ou senha incorreto')
+      }
+
+    }
+  });
+
+})
 
 // FUNÇÕES PARA CADASTRO
 // FUNÇÕES PARA CADASTRO
 
 app.post('/cadastro', (req, res) => {
-    let nome = req.body.nome;
-    let numero = req.body.numero;
-    let cpf = req.body.cpf;
-    let email = req.body.email;
-    let senha = req.body.senha;
+  let nome = req.body.nome;
+  let numero = req.body.numero;
+  let cpf = req.body.cpf;
+  let email = req.body.email;
+  let senha = req.body.senha;
 
-    connection.query("INSERT INTO `cliente`(`nome`, `numero`, `cpf`, `email`,`senha`) VALUES  ('" + nome + "','" + numero + "','" + cpf + "','" + email + "','" + senha + "')", function (err, rows, fields) {
-        console.log("Results:", rows);
-        if (!err) {
-            console.log("Cadastro feito com sucesso!!");
-            res.sendFile(__dirname + '/pages/recout_login.html')
-        } else {
-            console.log("Erro: Consulta não realizada", err);
-            res.send('Login failed');
-        }
-    });
+  connection.query("INSERT INTO `cliente`(`nome`, `numero`, `cpf`, `email`,`senha`) VALUES  ('" + nome + "','" + numero + "','" + cpf + "','" + email + "','" + senha + "')", function (err, rows, fields) {
+    console.log("Results:", rows);
+    if (!err) {
+      console.log("Cadastro feito com sucesso!!");
+      res.sendFile(__dirname + '/pages/recout_login.html')
+    } else {
+      console.log("Erro: Consulta não realizada", err);
+      res.send('Login failed');
+    }
+  });
+});
+
+// PUXANDO INFOS DO BANCO
+// PUXANDO INFOS DO BANCO
+// PUXANDO INFOS DO BANCO
+
+app.get('/getProductInfo/:id', (req, res) => {
+  const productId = req.params.id;
+  connection.query(`SELECT * FROM equipamento WHERE id_equipamento = ${productId}`, (err, rows, fields) => {
+    if (!err) {
+      const product = {
+        info: rows
+      };
+      console.log(product);
+      res.json(product);
+    } else {
+      console.log("Erro: Consulta não realizada", err);
+      res.status(500).json({ error: 'Erro no servidor' });
+    }
+  });
+});
+
+app.get('/getProducts', (req, res) => {
+  connection.query('SELECT * FROM kp_products', (err, rows, fields) => {
+    if (!err) {
+      const product = {
+        info: rows
+      };
+      res.json(product);
+    } else {
+      console.log("Erro: Consulta não realizada", err);
+      res.status(500).json({ error: 'Erro no servidor' });
+    }
+  });
 });
 
 app.listen(3000, () => {
-    console.log('Servidor rodando na porta 3000!')
+  console.log('Servidor rodando na porta 3000!')
 })
